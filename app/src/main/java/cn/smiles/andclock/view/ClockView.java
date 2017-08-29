cb2092d4557ac8c33f5272533d55bce9d726f2e7
Apples_kk <937690711@qq.com>
2017-08-29 19:26:03 +0800

    Merge branch 'master' of https://git.coding.net/qq937690711/AndClock
    add ClockView

--- app/src/main/java/cn/smiles/andclock/view/ClockView.java
+++ app/src/main/java/cn/smiles/andclock/view/ClockView.java
@@ -8,6 +8,8 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.Calendar;
+
 public class ClockView extends View {
 
     private Paint paint;
@@ -34,7 +36,16 @@ public class ClockView extends View {
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeCap(Paint.Cap.ROUND);
         paint.setStrokeJoin(Paint.Join.ROUND);
+
+        this.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                invalidate();
+                postDelayed(this, 1000);
+            }
+        }, 1000);
     }
+
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -74,13 +85,67 @@ public class ClockView extends View {
         }
         canvas.restore();
 
-        paint.setStrokeWidth(8);
         paint.setStrokeCap(Paint.Cap.ROUND);
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE);
+        int second = calendar.get(Calendar.SECOND);
+        System.out.println("hour= " + hour + ",minute=" + minute + ",second=" + second);
         //时针
-        canvas.drawLine(0, 0, 0, -120, paint);
+        paint.setStrokeWidth(8);
+        float[] hourps = calculatePoint(hour * 30, 100);
+        canvas.drawLines(hourps, paint);
         //分针
-        canvas.drawLine(0, 0, 140, 0, paint);
+        paint.setStrokeWidth(6);
+        float[] minuteps = calculatePoint(minute * 6, 130);
+        canvas.drawLines(minuteps, paint);
         //秒针
-        canvas.drawLine(0, 0, 0, 160, paint);
+        paint.setStrokeWidth(4);
+        float[] secondps = calculatePoint(second * 6, 160);
+        canvas.drawLines(secondps, paint);
+
+        paint.setStrokeWidth(2);
+        paint.setTextSize(24);
+        paint.setTextAlign(Paint.Align.CENTER);
+        for (int n = 1; n <= 12; n++) {
+            double theta = (n - 3) * (Math.PI * 2) / 12;
+            float x = (float) (240 * 0.7 * Math.cos(theta));
+            float y = (float) (240 * 0.7 * Math.sin(theta));
+            canvas.drawText(String.valueOf(n), x, y, paint);
+        }
     }
+
+    /**
+     * 根据角度和长度计算线段的起点和终点的坐标
+     *
+     * @param angle
+     * @param length
+     * @return
+     */
+    private float[] calculatePoint(float angle, float length) {
+        float[] points = new float[4];
+        if (angle <= 90f) {
+            points[0] = -(float) Math.sin(angle * Math.PI / 180) * 20;
+            points[1] = (float) Math.cos(angle * Math.PI / 180) * 20;
+            points[2] = (float) Math.sin(angle * Math.PI / 180) * length;
+            points[3] = -(float) Math.cos(angle * Math.PI / 180) * length;
+        } else if (angle <= 180f) {
+            points[0] = -(float) Math.cos((angle - 90) * Math.PI / 180) * 20;
+            points[1] = -(float) Math.sin((angle - 90) * Math.PI / 180) * 20;
+            points[2] = (float) Math.cos((angle - 90) * Math.PI / 180) * length;
+            points[3] = (float) Math.sin((angle - 90) * Math.PI / 180) * length;
+        } else if (angle <= 270f) {
+            points[0] = (float) Math.sin((angle - 180) * Math.PI / 180) * 20;
+            points[1] = -(float) Math.cos((angle - 180) * Math.PI / 180) * 20;
+            points[2] = -(float) Math.sin((angle - 180) * Math.PI / 180) * length;
+            points[3] = (float) Math.cos((angle - 180) * Math.PI / 180) * length;
+        } else if (angle <= 360f) {
+            points[0] = (float) Math.cos((angle - 270) * Math.PI / 180) * 20;
+            points[1] = (float) Math.sin((angle - 270) * Math.PI / 180) * 20;
+            points[2] = -(float) Math.cos((angle - 270) * Math.PI / 180) * length;
+            points[3] = -(float) Math.sin((angle - 270) * Math.PI / 180) * length;
+        }
+        return points;
+    }
+
 }
